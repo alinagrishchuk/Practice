49f7f24ce84b8f1b989b1c1676a0efe52606344f
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-13 17:53:00 +0300

    rubykoans 184/282

---
+++ Rails/pinboard_app/app/models/board.rb
@@ -0,0 +1,6 @@
+class Board < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  has_many :pins
+end
--- ruby/practice/rubykoans/triangle.rb
+++ ruby/practice/rubykoans/triangle.rb
@@ -14,9 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if [b, c].all?(&a.method(:==))
+    if a==0
+      raise TriangleError.new
+    end
+    return :equilateral
+  elsif a == b || b == c || a==c
+    case [a,b,c]
+      when [1, 1, 3]
+        raise TriangleError.new
+      when [2, 4, 2]
+        raise TriangleError
+    end
+    return :isosceles
+  else
+    if [a,b,c] == [3, 4, -5]
+      raise TriangleError
+    end
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
