165c6320fd39c5ce22157c3d956921754e3010fb
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-15 13:52:45 +0300

    practice with auto-complete association

--- Rails/ActionView/db/schema.rb
+++ Rails/ActionView/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614183811) do
+ActiveRecord::Schema.define(version: 20160615100613) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,5 +36,15 @@ ActiveRecord::Schema.define(version: 20160614183811) do
     t.datetime "updated_at",             null: false
     t.string   "state"
   end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
 end
