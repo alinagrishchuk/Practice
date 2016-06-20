6c0fb0f65e8fb89dd32e14216dd6d90d05386aaf
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-20 12:40:04 +0300

    setup and style project

---
+++ Rails/portfolio_app/app/controllers/projects_controller.rb
@@ -0,0 +1,45 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.order("created_at desc").paginate(page: params[:page], per_page: 10)
+  end
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+    if @project.save
+      redirect_to @project,  notice: 'post created!'
+    else
+      render 'new', notice: 'unable to save!'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project,  notice: 'project updated!'
+    else
+      render 'edit', notice: 'unable to save!'
+    end
+  end
+  def destroy
+    @project.destroy
+    redirect_to projects_path,  notice: 'project deleted!'
+
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description, :link, :slug)
+  end
+  def find_project
+    @project = Project.friendly.find(params[:id])
+  end
+end
--- Rails/portfolio_app/db/seeds.rb
+++ Rails/portfolio_app/db/seeds.rb
@@ -1,6 +1,13 @@
 if Post.count == 0
+  puts 'add posts'
   300.times do
-    puts 'add posts'
     Post.create!(title: Faker::Hipster.sentence, content: Faker::Hipster.paragraphs(4).join("\n"))
+  end
+end
+
+if Project.count == 0
+  puts 'add project'
+  300.times do
+    Project.create!(title: Faker::App.name, description: Faker::Hipster.paragraphs(2).join("\n"), link: "http://localhost:3000/")
   end
 end
